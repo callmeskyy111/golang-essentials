6c9b6e078ca2d9e65d03c5ad76ff75480269f44b
Soumadip <39thdecember@gmail.com>
2025-06-25 17:36:55 +0530

    Hello World And Basic Data Types

---
+++ 2_simple_values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+	//int
+	fmt.Println(1)
+
+	//string
+	fmt.Println("This is a string!")
+
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//floats
+	fmt.Println((10.6))
+
+	//Ops.
+	fmt.Println(7.5/2.8)
+}
