7171aa126e7d3b31751bc7452b7b6f4b6ef93b81
Soumadip <39thdecember@gmail.com>
2025-07-06 17:41:42 +0530

    Loops And Switch-case☑️

--- bank.go
+++ bank.go
@@ -2,58 +2,62 @@ package main
 
 import "fmt"
 
-// control structures
+// control structures, loops, switch-cases
 
 func main(){
 
 var accBalance = 1000.0
 
 fmt.Println("WELCOME to GoBank 🏦!")
-fmt.Println("Your initial-amount is: $",accBalance)
-fmt.Println("What do you want to do?")
-fmt.Println("1️⃣. Check balance")
-fmt.Println("2️⃣. Deposit")
-fmt.Println("3️⃣. Withdraw")
-fmt.Println("4️⃣. Exit")
 
-var choice int
-fmt.Print("Your choice: ")
-fmt.Scan(&choice)
+// for i:=0; i<2; i++{ ❌ // Not needed here..
+// ♾️ loop ☑️
+	for{
+	fmt.Println("\nYour amount is: $",accBalance)
+	fmt.Println("What do you want to do?")
+	fmt.Println("1️⃣. Check balance")
+	fmt.Println("2️⃣. Deposit")
+	fmt.Println("3️⃣. Withdraw")
+	fmt.Println("🔘.OTHER - Exit")
+	
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
 
-// wantsCheckBalance:= choice ==1
-
-if choice == 1{
-fmt.Println("Your balance is: $",accBalance)
-} else if choice == 2 {
-fmt.Print("💰 How much do you wanna deposit?: +$")
-var depositAmt float64 // local scope
-fmt.Scan(&depositAmt)
-if depositAmt <=0{
-	fmt.Println("INVALID AMOUNT!.. AMOUNT must not be -ve")
-	return //exits the program
-}
-accBalance+=float64(depositAmt)
-formattedBalance:= fmt.Sprintf("%.2f",accBalance)
-fmt.Println("Deposited ✅.. Your updated account-balance: $",formattedBalance)
-}else if choice == 3{
-	fmt.Print("💰 How much do you wanna withdraw?: -$")
-	var withdrawAmt float64 // local scope
-    fmt.Scan(&withdrawAmt)
-	if withdrawAmt <=0{
-		fmt.Println("INVALID AMOUNT!.. AMOUNT must not be -ve")
-		return //exits the program
+// Switch - Alternative to if-else,if,else etc.	
+	switch choice{
+	case 1:
+			fmt.Println("Your balance is: $",accBalance)
+	case 2:
+		fmt.Print("💰 How much do you wanna deposit?: +$")
+		var depositAmt float64 // local scope
+		fmt.Scan(&depositAmt)
+		if depositAmt <=0{
+			fmt.Println("INVALID AMOUNT!.. AMOUNT must not be -ve")
+			continue
+		}
+		accBalance+=float64(depositAmt)
+		formattedBalance:= fmt.Sprintf("%.2f",accBalance)
+		fmt.Println("Deposited ✅.. Your updated account-balance: $",formattedBalance)
+	case 3:
+		fmt.Print("💰 How much do you wanna withdraw?: -$")
+		var withdrawAmt float64 // local scope
+		fmt.Scan(&withdrawAmt)
+		if withdrawAmt <=0{
+			fmt.Println("INVALID AMOUNT!.. AMOUNT must not be -ve")
+			continue
+		}
+		if withdrawAmt>accBalance{
+			fmt.Println("Insufficient Balance :(")
+			continue
+		}
+		accBalance-=float64(withdrawAmt)
+		formattedBalance:= fmt.Sprintf("%.2f",accBalance)
+		fmt.Println("Amount withdrawn ✅.. Your updated account-balance: $",formattedBalance)
+	default:
+	fmt.Println("Good Day! Thanks for choosing GoBank")
+	return
+	//break
 	}
-	if withdrawAmt>accBalance{
-		fmt.Println("Insufficient Balance :(")
-		return
-	}
-	accBalance-=float64(withdrawAmt)
-	formattedBalance:= fmt.Sprintf("%.2f",accBalance)
-	fmt.Println("Amount withdrawn ✅.. Your updated account-balance: $",formattedBalance)
-}else{
-	fmt.Println("Good Day!")
 }
-
-fmt.Println("You chose: ",choice)
-
 }
